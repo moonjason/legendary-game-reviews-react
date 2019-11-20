319352bc9c4e29f68c0767fedf9bb244c96f150e
hassen <hasansaid51@gmail.com>
2019-11-19 21:24:33 -0800

    blueprint

--- src/Components/GamesContainer/index.js
+++ src/Components/GamesContainer/index.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 class GameContainer extends Component{
   state = {
     //game stuff
+    //
   }
   componentDidMount(){
     //call the getGame function here
@@ -14,7 +15,7 @@ class GameContainer extends Component{
   }
   //addGame function?
   //deleteGame function?
-  
+
   render(){
     return(
       <h1>GameContainer</h1>
