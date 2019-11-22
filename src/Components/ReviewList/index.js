35deda9f1de39b47582e49b775dd52aa1ed00218
Jason Moon <jasonmoonjm@gmail.com>
2019-11-22 09:18:07 -0800

    review list

--- src/Components/ReviewList/index.js
+++ src/Components/ReviewList/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {
+   Review
+} from './style'
+
 
 const ReviewList = (props) => {
   const reviews = props.foundReviews.map(review => {
     if (props.gameId === review.game_id) {
       return (
-        <div class="review" key={review.id}>
+        <Review key={review.id}>
           <h1>{review && review.title}</h1>
           <h3>{review && review.body}</h3>
+          {review.is_poitive ? <div>positive</div> : <div>negative</div>}
           {review.user_id.id === props.currentUser.id 
             ? <>
               <button>Edit</button> 
@@ -14,7 +19,7 @@ const ReviewList = (props) => {
               </>
             : ""
           }
-        </div>
+        </Review>
       )
     }
   })
---
+++ src/Components/ReviewList/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Review = styled.div`
+    color: white;
+    padding: 1% 0;
+    display: flex;
+    flex-direction: column;
+`
